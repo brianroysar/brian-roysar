78fb9e04491f9b70b4b65626e86d42b5db80bf4f
brianroysar <brian.sinambela1@gmail.com>
2023-06-14 23:46:55 -0700

    started experience styling

--- src/Experience.js
+++ src/Experience.js
@@ -1,24 +1,30 @@
 import React, { Component } from "react";
 import "./Experience.css";
-import profile_pic from "./profilepic.jpg";
-import Navbar from "./navbar.js";
+import "./home.css";
+
+const Card = () => {
+	return (
+		<div className="card">
+			<div className="left">
+				<h1 className="card-title">Yahoo</h1>
+				<h3 className="card-date"> June 2023 - Present </h3>
+			</div>
+			<div className="right">
+				<h3 className="card-role">Software Engineering Intern</h3>
+				<h3 className="card-desc">Working on the ads team</h3>
+				<h3 className="card-tech">C++</h3>
+			</div>
+		</div>
+	);
+};
 
 class Experience extends Component {
 	render() {
 		return (
 			<div className="main-page">
-				<div className="center-card">
-					<img src={profile_pic} alt="Logo" className="profile-pic" />
-					<div className="blurb-box">
-						<h1 className="welcome-message"> Hi! My name is </h1>
-						<h1 className="brian-roysar-name"> BRIAN 2</h1>
-						<h1 className="blurb">
-							Currently at UCLA pursuing a double major in Computer Science and
-							Applied Mathematics. Lost in the cross-roads between my left and
-							right brain.
-						</h1>
-					</div>
-				</div>
+				<Card />
+				<Card />
+				<Card />
 			</div>
 		);
 	}
